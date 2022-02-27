4a260992075eccaa04eae5f7890d7e50bd907203
Khuong Tran <tphuockhuong@gmail.com>
2022-02-27 08:05:56 +0700

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import "../styles/globals.css";
+import "@fontsource/plus-jakarta-sans";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ src/components/TopNav.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TopNav = () => {
+  return <div className="px-12 py-6">Estatery</div>;
+};
+
+export default TopNav;
